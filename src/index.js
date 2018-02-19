52f549656803e0638dd2b55708953db3204f66e5
mikefab <mikefabrikant@gmail.com>
2018-02-18 20:58:20 -0500

    Color in feature properties

--- src/index.js
+++ src/index.js
@@ -67,33 +67,6 @@ function scaleMatrix(matrix, scaleX, scaleY) {
   matrix[7] *= scaleY;
 }
 
-function assign_speed_value(properties) {
-  let slider = 3
-  let value = null;
-  if (properties.speed_connectivity !== null) {
-    value = properties.speed_connectivity;
-  } else if (properties.type_connectivity != null) {
-    value = properties.type_connectivity;
-  }
-
-  if (typeof value === 'undefined') {
-    // return '#6A1E74';
-    return [106, 30, 116]
-  } else if (value === 0 || value === 'No Service') {
-    // return '#d9534f';
-    return [217, 83, 79]
-  } else if (value >= slider || value === '3G') {
-    // return '#5cb85c';
-    return [92, 184, 92]
-  } else if (value < slider || value === '2G') {
-    // return '#F5A623';
-    return [245, 166, 35]
-  } else {
-    // return '#DCDCDC';
-    return [1, 1, 1]
-  }
-}
-
 const prepare_points = function(features, zoom) {
   let point_xy = new Float32Array(2 * features.length);
   let point_on_screen_color = new Float32Array(4 * features.length);
@@ -101,7 +74,8 @@ const prepare_points = function(features, zoom) {
   let point_size = new Float32Array(features.length);
   features.forEach((f, i) => {
 
-    var speed_value = assign_speed_value(f.properties)
+    var speed_value = f.properties.color
+    console.log('bbbbb')
 
     const lat = f.geometry.coordinates[1];
     const lon = f.geometry.coordinates[0];
